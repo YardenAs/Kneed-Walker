4b77787063350f3fdfb83f0c1e167241a70424e8
YardenAs <yarden.a.a@gmail.com>
2017-06-02 19:54:09 +0300

    First iteration of KneedWalker
    1. Finished writing the base code of kneedwalker. Most probably it's full of bugs.
    2. DynEq - gives the right dynamic equations
    3. EventMat - gives the right matrices for each event

--- @KneedWalker/KneedWalker.m
+++ @KneedWalker/KneedWalker.m
@@ -2,32 +2,33 @@ classdef KneedWalker
     % KneedWalker is a class that defines a kneed biped robot.
 
     properties
-        shin  = [];   % mass, length, moment of inertia
-        thigh = [];   % mass, length, moment of inertia
-        torso = [];   % mass, length, moment of inertia
-        g = 9.81;
+        sh = [];    % shank mass, length, moment of inertia
+        th = [];    % thigh mass, length, moment of inertia
+        to = [];    % torso mass, length, moment of inertia
+        g  = 9.81;
 
         % Support leg coordinates
         xS = 0;
         yS = 0;
         
-        % Support Leg
-        Support = 2;
-        
         % enum
         Left  = 1;
         Right = 2;
-       
+        
+        % Support Leg and Phase
+        Support = 2;
+        Phase = 'KneesFree'; % SKneeFree, NSKneeFree, KneesLock
+        
         % Control torques
-        Torques = [0 0 0 0].';
+        Torques = [0 0 0 0].'; % Ship, NShip, Sknee, NSknee
         
         % Event index
         nEvents = 4;
         iEvent  = 4;  
         % 1 - leg contact
-        % 2 - knee locked
-        % 3 - To Be Decided
-        % 4 - To Be Decided
+        % 2 - support knee locked
+        % 3 - non support knee locked
+        % 4 - T.B.D (knee lock releases???)
     end
     
     
@@ -36,88 +37,177 @@ classdef KneedWalker
         function KW = KneedWalker(varargin)
             switch nargin
                 case 0
-                    KW;%#ok<VUNUS>
+                KW;%#ok<VUNUS>
                 case 3
-                    KW.shin  = varargin{1};
-                    KW.thigh = varargin{2};
-                    KW.torso = varargin{3};
-                    KW.xS = 0;
-                    KW.yS = 0;
+                if (length(varargin{1}) ~= 3) || (length(varargin{2}) ~= 3) ...
+                    || (length(varargin{3}) ~= 3)
+                error('Robot parameters aren''t valid');
+                else
+                KW.sh  = varargin{1};
+                KW.th = varargin{2};
+                KW.to = varargin{3};
+                KW.xS = 0;
+                KW.yS = 0;
+                end
                 case 4
-                    KW.shin  = varargin{1};
-                    KW.thigh = varargin{2};
-                    KW.torso = varargin{3};
-                    KW.xS = getPos(init);  %%% gets the initial position of the 
-                    KW.yS = getPos(init);  %%% the support leg T.B.D
+                if (length(varargin{1}) ~= 3) || (length(varargin{2}) ~= 3) ...
+                    || (length(varargin{3}) ~= 3)
+                error('Robot parameters aren''t valid');
+                else
+                KW.sh  = varargin{1};
+                KW.th = varargin{2};
+                KW.to = varargin{3};
+                KW.xS = getPos(init);  %%% gets the initial position of the 
+                KW.yS = getPos(init);  %%% the support leg T.B.D
+                end
                 otherwise
-                    error('Couldn'' construct KneedWalker object, check your input');
+                    error('Couldn''t construct KneedWalker object, check your input');
+            end
+        end
+               
+        function [x, y] = GetPos(KW, X, which)
+            switch which
+                case 'Sankle'
+                x = KW.xS;
+                y = KW.yS;
+                case 'Sknee'
+                rs1 = [X(1) + KW.th(2)*sin(X(7)), X(3) - KW.th(2)*cos(X(7))];
+                x = rs1(1);
+                y = rs1(2);
+                case 'NSankle'
+                rns1 = [X(1) - KW.sh(2)*sin(X(9)), X(3) - KW.th(2)*cos(X(9))];  % non support knee          
+                rns2 = rns1 + [- KW.sh(2)*sin(X(13)),- KW.th(2)*cos(X(13))];    % non support ankle
+                x = rns2(1);
+                y = rns2(2);
+                case 'NSknee'
+                rns1 = [X(1) - KW.sh(2)*sin(X(9)), X(3) - KW.th(2)*cos(X(9))];
+                x = rns1(1);
+                y = rns1(2);
+                case 'TorsoCOM'
+                rt = [X(1) + KW.to(2)/2*sin(X(5)), X(3) + KW.to(2)/2*cos(X(5))];
+                x = rt(1);
+                y = rt(2);
+                otherwise
+                    error('No such position');
             end
         end
         
+        function [xdot, ydot] = GetVel(KW, X, which)
+            switch which
+                case 'Sankle'
+                xdot = X(2) + (X(12)*KW.sh(2)*cos(X(11))) + X(8)*KW.th(2)*cos(X(7));
+                ydot = X(4) + (X(12)*KW.sh(2)*sin(X(11))) + X(8)*KW.th(2)*sin(X(7));                
+                case 'Sknee'
+                xdot = X(2) + X(8)*KW.th(2)*cos(X(7));
+                ydot = X(4) + X(8)*KW.th(2)*sin(X(7));
+                case 'NSankle'
+                xdot = X(2) - (X(14)*KW.sh(2)*cos(X(13))) - X(10)*KW.th(2)*cos(X(9));
+                ydot = X(4) + (X(14)*KW.sh(2)*sin(X(13))) + X(10)*KW.th(2)*sin(X(9));
+                case 'NSknee'
+                xdot = X(2) - X(10)*KW.th(2)*cos(X(9));
+                ydot = X(4) + X(10)*KW.th(2)*sin(X(9));
+                case 'TorsoCOM'
+                xdot = X(2) + X(6)*KW.to(2)*cos(X(5))/2;
+                ydot = X(4) - X(6)*KW.to(2)*sin(X(5))/2;
+                otherwise
+                    error('No such position');
+            end
+        end
         
-        function [x, y] = GetPos(KW, X, which) %%% T.B.D
-        if strcmp(which, 'S')
-            x = KW.xS;
-            y = KW.yS;
-            return;
-        end
-        if strcmp(which, 'NS')
-            x = KW.xNS;
-            y = KW.yNS;  %%% T.B.D %%%
-            return;
-        end    
+        function [F] = GetReactionForces(KW, X)
+            [M, B, G, W, Wdot, Fq] = DynEq(KW, X);
+            dq = [X(2), X(4), X(6), X(8), X(10), X(12), X(14)].';
+            A = [M -W.'; W zeros(2,2)];
+            b = [Fq - B - G; -Wdot*dq];
+            sol = A\b;
+            F = [sol(8) end].';
         end
         
-        function [xdot, ydot] = GetVel(KW, X, which) %%%T.B.D
-        end
-        
-        function [F] = GetGRF(KW,X) %%%T.B.D
-        F = zeros(2,1);
-        end
-        
-        function [Xdot] = Derivative(KW, t, X, iEvents)
-        switch iEvents
-            case 1
-                %%% Xdot = something
-            case 2
-                %%% Xdot = something else
-        end
+        function [Xdot] = Derivative(KW, t, X) %#ok<INUSL>
+            [M, B, G, W, Wdot, Fq] = DynEq(KW, X);
+            dq = [X(2), X(4), X(6), X(8), X(10), X(12), X(14)].';
+            A = [M -W.'; W zeros(size(W))];
+            b = [Fq - B - G; -Wdot*dq];
+            sol = A\b;
+            Xdot = [X(2) sol(1) X(4) sol(2) X(6) sol(3) X(8) sol(4)...
+                    X(10) sol(5) X(12) sol(6) X(14) sol(7)].';      
         end
         
         function [value, isterminal, direction] = Events(KW, X, Floor)
             value = ones(KW.nEvents,1);
             isterminal = ones(KW.nEvents,1);
-            direction = ones(KW.nEvents,1);
+            direction = [-1 1 1 0]; %% CHECK
             
-            value(1) = yNS - Floor.Surf(xNS);
-            %%% value(2) = knee lock
+            yNS = KW.GetPos(X, 'NSankle');
+            value(1) = yNS - Floor.Surf(xNS); % ground contact
+            if strcmp(KW.Phase, 'KneesFree') || strcmp(KW.Phase, 'SKneeFree')
+                value(2) = X(11) - X(7);          % support knee lock
+            end
+            if strcmp(KW.Phase, 'KneesFree') || strcmp(KW.Phase, 'NSKneeFree')
+                value(3) = X(9) - X(13);          % non support knee lock
+            end
         end
         
-        function [KW, Xa] = HandleEvent(KW, iEvent, Xb, t)
-        switch iEvent
-            case 1 % Leg contact
-                %%% Xa = %%% Impact calculation
-                if KW.Support == KW.Left
-                   KW.Support = KW.Right;
-                elseif KW.Support == KW.Right
-                       KW.Support = KW.Left;
-                end
-                % Update support foot position
-                [xNS, yNS] = KW.GetPos(Xb,'NS');
-                KW.yS = yNS;
-                KW.xS = xNS;
+        function [Xa, LAMBDA] = CalcImpact(KW, Xb)
+            [M, W] = EventMat(KW, Xb);
+            A = [M -W.'; W zeros(size(W))];
+            b = [-M*Xb, zeros(size(W,1))];
+            sol = A\b;
+            Xa = sol(1:7);
+            LAMBDA = sol(8:end);
+            %%% here get event asummps GetEventAssumps = something %%%
+        end
+       
+        function [KW, Xa] = HandleEvent(KW, iEvent, Xb)
+            switch iEvent
+                case 1 % ground contact
+                     Xa = CalcImpact(KW, Xb);
+                    % Update support leg
+                    if KW.Support == KW.Left
+                    KW.Support = KW.Right;
+                    elseif KW.Support == KW.Right
+                        KW.Support = KW.Left;
+                    end
+                    % Update support foot position
+                    [xNS, yNS] = KW.GetPos(Xb,'NS');
+                    KW.yS = yNS;
+                    KW.xS = xNS;
+                    % Update walking phase
+                    if strcmp(KW.Phase, 'KneesLocked')
+                        KW.Phase = 'NSKneeFree'; % otherwise - this is a biped without knees
+                    end
+                    if strcmp(KW.Phase, 'SKneeFree')
+                        KW.Phase = 'NSKneeFree';
+                    end
+                    if strcmp(KW.Phase, 'NSKneeFree')
+                        KW.Phase = 'SKneeFree';
+                    end
+                case 2 % support knee locked
+                    Xa = CalcImpact(KW, Xb);
+                    % Update walking phase
+                    if strcmp(KW.Phase, 'KneesFree')
+                        KW.Phase = 'NSKneeFree';
+                    end
+                    if strcmp(KW.Phase, 'SKneeFree')
+                        KW.Phase = 'KneesLocked';
+                    end
+                case 3 % non support knee locked
+                    Xa = CalcImpact(KW, Xb);
+                    % Update walking phase
+                    if strcmp(KW.Phase, 'KneesFree')
+                        KW.Phase = 'SKneeFree';
+                    end
+                    if strcmp(KW.Phase, 'NSKneeFree')
+                        KW.Phase = 'KneesLocked';
+                    end
+            end
+        end
         
-%                 % Compute velocity
-%                 dT = t-KW.last_t;
-%                 KW.curSpeed = KW.GetStepLength(Xb)/dT;
-%                 KW.last_t = t;
-                
-            case 2 % Knee Lock
-                %%% T.B.D      
+        function CB = SetTorques(CB,T)
+            CB.Torques=T;
         end
-        end
-    end     
-end
+    end
+end     
 
 
 
--- KneedWalker/DynamicEqsDerivation.m
+++ KneedWalker/DynamicEqsDerivation.m
@@ -1,7 +1,7 @@
 % Torso parameters
 syms mt lt It real
  
-% Shin parameters
+% shank parameters
 syms msh lsh Ish real
  
 % Thigh parameters
@@ -30,8 +30,9 @@ ddq = [ddx ddy dda ddgs ddgns ddbs ddbns].';
 rt = [x + lt/2*sin(a), y + lt/2*cos(a)];
 rs1 = [x + lth/2*sin(gs), y - lth/2*cos(gs)];    % support thigh
 rns1 = [x - lth/2*sin(gns), y - lth/2*cos(gns)]; % non support thigh
-rs2 = rs1 + [lth/2*sin(gs) + lsh/2*sin(bs),-lth/2*cos(gs) - lsh/2*cos(bs)];        % support shin
-rns2 = rns1 + [-lth/2*sin(gns) - lsh/2*sin(bns),-lth/2*cos(gns) - lsh/2*cos(bns)]; % non support shin
+rs2 = rs1 + [lth/2*sin(gs) + lsh/2*sin(bs),-lth/2*cos(gs) - lsh/2*cos(bs)];        % support shank
+rns2 = rns1 + [-lth/2*sin(gns) - lsh/2*sin(bns),-lth/2*cos(gns) - lsh/2*cos(bns)]; % non support shank
+
 
 % Center of masses velocities (using chain rule)
 vt   = jacobian(rt,q)*dq;
@@ -43,18 +44,18 @@ vns2 = jacobian(rns2,q)*dq;
 %%% Kinetic energy %%%
 
 % Torso
-ket = 1/2*mt*(vt.'*vt) + 1/2*It*a^2;
+ket = 1/2*mt*(vt.'*vt) + 1/2*It*da^2;
 
 % Support leg
-ke_support_thigh = 1/2*mth*(vs1.'*vs1) + 1/2*Ith*gs^2;
-ke_support_shin  = 1/2*msh*(vs2.'*vs2) + 1/2*Ish*bs^2;
+ke_support_thigh = 1/2*mth*(vs1.'*vs1) + 1/2*Ith*dgs^2;
+ke_support_shank  = 1/2*msh*(vs2.'*vs2) + 1/2*Ish*dbs^2;
 
 % Non support leg
-ke_nsupport_thigh = 1/2*mth*(vns1.'*vns1) + 1/2*Ith*gns^2;
-ke_nsupport_shin  = 1/2*msh*(vns2.'*vs2) + 1/2*Ish*bns^2;
+ke_nsupport_thigh = 1/2*mth*(vns1.'*vns1) + 1/2*Ith*dgns^2;
+ke_nsupport_shank  = 1/2*msh*(vns2.'*vs2) + 1/2*Ish*dbns^2;
 
-KE = simplify(ket + ke_support_thigh + ke_support_shin + ke_nsupport_thigh...
-   + ke_nsupport_shin);
+KE = simplify(ket + ke_support_thigh + ke_support_shank + ke_nsupport_thigh...
+   + ke_nsupport_shank);
 
 %%% Potential energy %%%
 
@@ -62,24 +63,97 @@ KE = simplify(ket + ke_support_thigh + ke_support_shin + ke_nsupport_thigh...
 pet = mt*g*rt(2);
 
 % Support leg
-pe_support_thigh = mth*g*rs1;
-pe_support_shin = msh*g*rs2;
+pe_support_thigh = mth*g*rs1(2);
+pe_support_shank = msh*g*rs2(2);
 
 % Non support leg
-pe_nsupport_thigh = mth*g*rns1;
-pe_nsupport_shin = msh*g*rns2;
+pe_nsupport_thigh = mth*g*rns1(2);
+pe_nsupport_shank = msh*g*rns2(2);
 
-PE = simplify(pet + pe_support_thigh + pe_support_shin + pe_nsupport_thigh...
-   + pe_nsupport_shin);
+PE = simplify(pet + pe_support_thigh + pe_support_shank + pe_nsupport_thigh...
+   + pe_nsupport_shank);
 
 %%% Input forces power %%%
 
-hipSAng   = da + dgs;
+hipSAng   = -da - dgs;
 hipNSAng  = da - dgns;
-kneeSAng  = dgs - dbs;
+kneeSAng  = -dgs + dbs;
 kneeNSAng = dgns - dbns;
-relAngs = [hipSAng, hipNSAng, kneeSAang, kneeNSAng];
-P = InMoments*relAngs;
+relAngs = [hipSAng, hipNSAng, kneeSAng, kneeNSAng];
+P = relAngs*InMoments;
+Fq = jacobian(P,dq).';
+
+%%% Ground Constraints %%%
+
+supCon = rs2 + [lsh/2*sin(bs), -lsh/2*cos(bs)];
+W = jacobian(supCon,q);
+
+%%% Knees Constraints %%%
+
+SkneeLock  = bs - gs;
+NSkneeLock = gns - bns;
+W_k = jacobian([SkneeLock ,NSkneeLock],q);
+
+%%% System's matrices %%%
+
+G = jacobian(PE,q).'; % Potential energy vector
+M = jacobian(KE,dq);
+M = jacobian(M,dq); % Inertia matrix
+W = simplify([W;W_k]);
+GAMMA = 0;
+for i = 1:7
+    for j = 1:7
+        for k = 1:7
+            GAMMA = GAMMA + 1/2*(diff(M(i,j),q(k)) + diff(M(i,k),q(j))...
+                    - diff(M(k,j),q(i)))*dq(j)*dq(k);
+        end
+    end
+    B(i) = simplify(GAMMA); %#ok
+GAMMA = 0;
+end
+
+
+Wdot = W;
+n = size(W);
+for i = 1:n(1)
+Wdot(i,:) = (jacobian(W(i,:),q)*dq).';
+end
+
+Wdot = simplify(Wdot);
+B = simplify(B.');
+M = simplify(M);
+G = simplify(G);
+
+
+
+%%% Impact laws %%%%% 
+
+%% ground contact
+
+rnsa = rns1 + [-lth/2*sin(gns) - lsh*sin(bns),-lth/2*cos(gns) - lsh*cos(bns)];
+W = jacobian(rnsa,q);
+W = [W;W_k]; % depends on the robot's phase
+
+%% knee lock
+syms C
+C = sym(zeros(7,1));
+for i = 1:7
+    sub(i) = diff(KE,dq(i)); %#ok
+    for j = 1:7
+        subb = diff(sub(i),q(j));
+        C(i) = C(i) + subb*dq(j); 
+    end
+    C(i) = C(i) - diff(KE,q(i));
+end
+
+C = simplify(C);
+        
+    
+
+
+
+
+
 
 
 
